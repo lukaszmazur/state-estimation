dea6fcadc78a28d5c7f8fffd5281ec7e3418a184
Lukasz Mazur <lmazur89@gmail.com>
2024-01-27 23:04:44 +0100

    update comments

--- state_estimator.py
+++ state_estimator.py
@@ -23,8 +23,8 @@ class StateEstimator():
         self._h_jac[:, :3] = np.eye(3)  # measurement model jacobian
 
         self._q_var_const = np.eye(6)
-        self._q_var_const[:3,:] *= self._var_imu_f**2  # SOLUTION: this was not squared
-        self._q_var_const[3:,:] *= self._var_imu_w**2  # SOLUTION: this was not squared
+        self._q_var_const[:3,:] *= self._var_imu_f**2
+        self._q_var_const[3:,:] *= self._var_imu_w**2
 
         # state estimates
         self._p_est = np.zeros(3)  # position estimates
@@ -85,7 +85,7 @@ class StateEstimator():
         delta_t = imu_t - self._previous_prediction_timestamp
         self._previous_prediction_timestamp = imu_t
 
-        # 1. Update state with IMU inputs
+        # update state with IMU inputs
         c_ns = R.from_quat(self._q_est).as_matrix()
         rotated_imu_acc = c_ns.dot(imu_f)
         acceleration = rotated_imu_acc + self._g
@@ -97,16 +97,15 @@ class StateEstimator():
         delta_rotation = R.from_euler('xyz', delta_angles)
         q_check = (delta_rotation * R.from_quat(self._q_est)).as_quat()
 
-        # 1.1 Linearize the motion model and compute Jacobians
+        # linearize the motion model and compute Jacobians
         f_jac = np.eye(9)
         f_jac[:3, 3:6] = delta_t * np.eye(3)
         f_jac[3:6, 6:] = -delta_t * skew_symmetric(rotated_imu_acc)
 
-        # 2. Propagate uncertainty
+        # propagate uncertainty
         q_var = (delta_t**2) * self._q_var_const
         p_cov_check = f_jac @ self._p_cov @ f_jac.T + self._l_jac @ q_var @ self._l_jac.T
 
-        # Update states (save)
         self._p_est = p_check
         self._v_est = v_check
         self._q_est = q_check
@@ -130,24 +129,23 @@ class StateEstimator():
         v_check = self._v_est
         q_check = self._q_est
 
-        # 3.1 Compute Kalman Gain
-        r_cov = np.eye(3) * (sensor_var**2)  # SOLUTION: sensor var is not squared
+        # compute Kalman Gain
+        r_cov = np.eye(3) * (sensor_var**2)
         k_gain = p_cov_check @ h_jac.T @ np.linalg.inv(h_jac @ p_cov_check @ h_jac.T + r_cov)  # 9x3
 
-        # 3.2 Compute error state
+        # compute error state
         delta_x = k_gain @ (y_k - p_check)  # 9x1
 
-        # 3.3 Correct predicted state
+        # correct predicted state
         p_hat = p_check + delta_x[:3]
         v_hat = v_check + delta_x[3:6]
 
         delta_angles_r = R.from_euler('xyz', delta_x[6:])
         q_hat = (delta_angles_r * R.from_quat(q_check)).as_quat()
 
-        # 3.4 Compute corrected covariance
+        # compute corrected covariance
         p_cov_hat = (np.eye(9) - k_gain @ h_jac) @ p_cov_check
 
-        # Update states (save)
         self._p_est = p_hat
         self._v_est = v_hat
         self._q_est = q_hat
